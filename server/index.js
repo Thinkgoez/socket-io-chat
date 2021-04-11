6d7abbbbb7b516deeffdc519a81f13af22d7a100
Thinkgoez <thinkgoez@gmail.com>
2021-04-11 14:01:02 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const server = require('http').createServer(app);
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// })
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const io = require('socket.io')(server, {
     cors: {
